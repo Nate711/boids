8438aedb411a8a5ca1a514405914969169ed4381
Josh Pollock <treefish@nostarch.com>
2012-08-23 12:38:53 -0700

    Fixed the self-detection problem

--- Actually Hopefully Good Boids (2)/Assets/Scripts/Boid Behavior Laws.js
+++ Actually Hopefully Good Boids (2)/Assets/Scripts/Boid Behavior Laws.js
@@ -27,33 +27,40 @@ function Update () {
 }
 
 function FindNeighbors (){
-	// JOSH (this is Nathan) make sure neighbors doesn't include the boid
-	// I don't know enough about unity to answer this myself	
+	// Nathan: JOSH (this is Nathan) make sure neighbors doesn't include the boid
+	// I don't know enough about unity to answer this myself
 	
-	var boidCollidersInSphere : Collider[] = Physics.OverlapSphere(transform.position,radius,boidLayer);
-	Debug.Log(boidCollidersInSphere.Length);
-	var boidTransformsInSphere : Transform[] = new Transform[boidCollidersInSphere.length];
 	
-	for (var i = 0; i < boidTransformsInSphere.length; i++){
-		boidTransformsInSphere[i] = boidCollidersInSphere[i].transform;
-	}
-	/*var boidsArray : Transform[] = [];
+	// Josh: I tested it and it seems to find itself (see the new Debug.Log below). Thanks! Fixed the problem btw.
+	// Also I forget why I needed to make an array of the transforms so I'm commenting that out for now.
 	
-	for ( var col : Collider in objectsInSphere ){
-			if ( col.tag == "Boid" ){
-				boidsArray.Add(col.transform.root);
+	var boidCollidersInSphere : Collider[] = Physics.OverlapSphere(transform.position,radius,boidLayer);//get an array of all boids in radius of the Perception Sphere
+	//Debug.Log(boidCollidersInSphere.Length);
+	/*var boidTransformsInSphere : Transform[] = new Transform[boidCollidersInSphere.length];
+	
+	for (var i : int = 0; i < boidTransformsInSphere.length; i++){
+		//if (boidCollidersInSphere[i].transform.position == transform.position) Debug.Log("Found myself");
+		
+		if (boidCollidersInSphere[i].transform.position != transform.position) boidTransformsInSphere[i] = boidCollidersInSphere[i].transform;
+	}*/
+	
+	var boidCollidersWithoutMe : Collider[] = new Collider[boidCollidersInSphere.length];
+	
+	for (var i : int = 0; i < boidCollidersInSphere.length; i++){
+		if(boidCollidersInSphere[i].transform.position!=transform.position){ //make sure we don't include ourselves
+			boidCollidersWithoutMe[i]=boidCollidersInSphere[i];
 			}
 		}
-	var boids : Transform[];
-	boids = boidsArray;
-	return boids;*/
-	return boidCollidersInSphere;
+	
+	
+	return boidCollidersWithoutMe;
 }
 
 function Cohesion () {
 	//begin calculate the average position of boids in neighborhood
 	var cohesionVector : Vector3 = Vector3.zero;
 	for (var boid : Collider in boidsInNeighborhood){
+		if(!boid) continue; //if index is empty, move to next iteration (this is needed because we have an empty index in the array returned by FindNeighbors()
 		cohesionVector += boid.transform.position;
 	//Debug.Log(boid.transform.position);
 	}
